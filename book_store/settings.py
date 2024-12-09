8270f467a9beea195cc793f9a478ef8e284215c6
Ana Geguchadze <anageguchadze22@gmail.com>
2024-12-09 21:03:28 +0100

    edit postgres data

--- book_store/settings.py
+++ book_store/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bookstore',  
         'USER': 'postgres', 
-        'PASSWORD': 'Datunia22@',    
+        'PASSWORD': 'postgresadmin22',    
         'HOST': 'localhost',            
         'PORT': '5432',                  
     }
